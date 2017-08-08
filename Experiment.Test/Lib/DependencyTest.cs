74c4f86005eb826bab0337febe3e8f783e8d07cf
Harry Cahyadi <mail.harry.cahyadi@gmail.com>
2017-08-08 20:34:08 +1000

    Add unit test project

---
+++ Experiment.Test/Lib/DependencyTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experiment.Test.Lib
+{
+    [TestClass]
+    public class DependencyTest
+    {
+        [TestInitialize]
+        public void Initialise()
+        {
+
+        }
+
+        public void CanSwitchDependencyVersion()
+        {
+
+        }
+    }
+}
---
+++ Experiment.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Experiment.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Experiment.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("89d2d600-042c-4137-9d56-8d21bf986a93")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
